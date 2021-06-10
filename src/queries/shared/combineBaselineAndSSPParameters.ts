10d8cfbc43b8f4e13f99cc90852f3fd15cbf09bc
Paul Wand <66976845+SilentEsper@users.noreply.github.com>
2021-06-10 10:08:18 -0600

    Update combineBaselineAndSSPParameters.ts

--- src/queries/shared/combineBaselineAndSSPParameters.ts
+++ src/queries/shared/combineBaselineAndSSPParameters.ts
@@ -1,6 +1,6 @@
 import { SystemSecurityPlan, Profile } from "oscal"
 
-export const combineBaslineAndSSPParameters = (baseline: Partial<Profile>, ssp: Partial<SystemSecurityPlan>) => {
+export const combineBaslineAndSSPParameters = (baseline: Partial<Profile> = {}, ssp: Partial<SystemSecurityPlan> = {}) => {
     return [
         ...baseline?.modify?.set_parameters?.map(({ values, param_id }) => ({ param_id, values: values || [] })) || [],
         ...ssp?.control_implementation?.set_parameters || []]
